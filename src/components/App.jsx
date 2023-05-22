cb781239188e85a0777045a09f19caf37c98d107
pradumn <pradumnprasad883@gmail.com>
2023-05-22 18:19:50 +0530

    Fixed styling errors.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import "../styles.css";
 import CreateArea from "./CreatArea";
 import Edit from "./Edit"
 function App() {
